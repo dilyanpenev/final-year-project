39fbaa6914d244d6343d510839654540bc00e818
Dilyan Penev <dilyan.penev@student.manchester.ac.uk>
2023-02-22 19:12:10 +0000

    Style home page

--- src/js/views/HomePage.js
+++ src/js/views/HomePage.js
@@ -1,7 +1,23 @@
 function HomePage(props) {
     return (
-        <div>
-            <h1>Colourisation</h1>
+        <div className="home-page">
+            <div className="home-page__banner">
+                <h1>Colourise your black-and-white photos <br /> with the help of deep learning</h1>
+                <div className="home-page__banner__button">
+                    <h3>TRY IT</h3>
+                </div>
+            </div>
+            <div className="home-page__colour-mix-1"></div>
+            <div className="home-page__description">
+                <h2>What? Who? Why?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent turpis neque, laoreet in nisl non, pellentesque consequat est. Integer non tortor scelerisque, lobortis urna vel, finibus nulla. Quisque tempus orci tortor, ac porttitor massa eleifend sed. Praesent vehicula ultrices blandit. Duis id urna at nisi varius ultricies sit amet sit amet turpis. Aliquam imperdiet ullamcorper magna, sit amet convallis tortor facilisis vitae. Suspendisse vitae volutpat augue, et pretium purus. Duis vulputate turpis sed lobortis varius. Duis luctus consectetur orci, eget dignissim ipsum fringilla nec. Ut luctus lorem lacus.</p>
+                <p>Sed et orci congue, accumsan est eu, egestas arcu. Vivamus eget nisl at orci suscipit tempor in a nibh. Donec scelerisque lorem sit amet nisi porta, luctus mollis ligula aliquam. In hac habitasse platea dictumst. Duis semper justo ut euismod accumsan. Maecenas eu pretium odio. Etiam rhoncus nisi sed porta mattis. Donec cursus, risus a lobortis dapibus, sapien orci pulvinar enim, sed viverra felis purus ac mi. Mauris ut bibendum metus. In hac habitasse platea dictumst. Quisque imperdiet erat sed risus ullamcorper, non condimentum risus condimentum. Sed eget ipsum est.</p>
+            </div>
+            <div className="home-page__colour-mix-2"></div>
+            <div className="home-page__acknowledgements">
+                <h2>Acknowledgements</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent turpis neque, laoreet in nisl non, pellentesque consequat est. Integer non tortor scelerisque, lobortis urna vel, finibus nulla. Quisque tempus orci tortor, ac porttitor massa eleifend sed. Praesent vehicula ultrices blandit. Duis id urna at nisi varius ultricies sit amet sit amet turpis. Aliquam imperdiet ullamcorper magna, sit amet convallis tortor facilisis vitae. Suspendisse vitae volutpat augue, et pretium purus. Duis vulputate turpis sed lobortis varius. Duis luctus consectetur orci, eget dignissim ipsum fringilla nec. Ut luctus lorem lacus.</p>
+            </div>
         </div>
     );
 }
